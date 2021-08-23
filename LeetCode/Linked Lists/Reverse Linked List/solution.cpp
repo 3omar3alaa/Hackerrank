d0a9983fa2b4afff3fb7f66621b73c0bbefc28f8
Omar Alaa <omaralaa1996@gmail.com>
2021-08-23 09:10:54 +0200

    Trees

---
+++ LeetCode/Linked Lists/Reverse Linked List/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+        
+        if(head -> next == NULL)        
+            return head;
+        
+        ListNode* start = head;
+        ListNode* mid = head -> next;
+        ListNode* end = mid -> next;
+        
+        start -> next = NULL;
+        
+        while(end != NULL)
+        {
+            mid -> next = start;
+            start = mid;
+            mid = end;   
+            end = end -> next;
+        }    
+        
+        mid -> next = start;
+        return mid;
+    }
+};
