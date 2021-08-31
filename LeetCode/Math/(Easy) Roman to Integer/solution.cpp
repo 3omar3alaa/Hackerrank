707077a1da6bb77dc5661a6a9124ade25e6fd435
Omar Alaa <omaralaa1996@gmail.com>
2021-08-31 05:43:41 +0200

    Roman to Integer

---
+++ LeetCode/Math/(Easy) Roman to Integer/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        map<char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+      
+        int result = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(roman[s[i+1]] > roman[s[i]])
+            {
+                result += (roman[s[i+1]] - roman[s[i]]);
+                i++;
+            }
+            else
+                result += roman[s[i]];
+        }
+        return result;
+        
+    }
+};
