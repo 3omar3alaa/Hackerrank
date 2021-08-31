3a637fe8fd913e250a03a7220f9a88ee294094b3
Omar Alaa <omaralaa1996@gmail.com>
2021-08-31 07:59:07 +0200

    Reverse Bits

---
+++ LeetCode/Others/(Easy) Reverse Bits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        int result = 0;
+        for(int i = 0; i < 32; i++){
+            result = result << 1;
+            result = result | (n % 2);
+			/*
+			Another solution
+			result = result | (n & 1);
+			*/
+            n = n >> 1;          
+        }
+        return result;
+    }
+};
