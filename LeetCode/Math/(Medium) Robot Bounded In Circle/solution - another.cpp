a0e56d01d27b21c405b3ae6a1bb828c89f0fa24c
Omar Alaa <omaralaa1996@gmail.com>
2021-11-12 18:03:44 +0200

    Create solution - another.cpp

---
+++ LeetCode/Math/(Medium) Robot Bounded In Circle/solution - another.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        // repeat the instructions 4 times max to make sure it is a sqaure
+        
+        char direction = 'U';
+        int x = 0;
+        int y = 0;
+        
+        for(int j = 0; j < instructions.length(); j++){
+            if(instructions[j]== 'G'){
+                if(direction == 'U') y++;
+                else if(direction == 'R') x++;
+                else if(direction == 'D') y--;
+                else direction = x--;
+            }
+            else if(instructions[j] == 'L'){
+                if(direction == 'U') direction = 'L';
+                else if(direction == 'R') direction = 'U';
+                else if(direction == 'D') direction = 'R';
+                else direction = 'D';
+            }
+            else if(instructions[j] == 'R'){
+                if(direction == 'U') direction = 'R';
+                else if(direction == 'R') direction = 'D';
+                else if(direction == 'D') direction = 'L';
+                else direction = 'U';
+            }
+        }
+
+        return (x == 0 && y == 0) || direction != 'U';
+    }
+};
